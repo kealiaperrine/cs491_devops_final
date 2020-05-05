a60d0926473ad9cac3a340b1b24dfc70df161679
Kealia Perrine <39889309+kealiaperrine@users.noreply.github.com>
2020-05-05 09:43:38 -0700

    deleted uneeded function

--- sources/follow.py
+++ sources/follow.py
@@ -5,12 +5,16 @@ class Follow:
         return target
 
     def closeEnough(self, current, target):
-        diff = map(abs, target - current)
-
+        diff = [t - c for t, c in zip(target, current)]
+        if diff[0] < 0:
+            diff[0] *= -1
+        if diff[1] < 0:
+            diff[1] *= -1
+        if diff[2] < 0:
+            diff[2] *= -1
         if diff > [1,1,1]:
             return False
         else:
             return True
 
-    def whichWay(self, current, target):
-        return target - current
+
